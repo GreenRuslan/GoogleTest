285e5fad7cb87ab2d11a81865dbf031fee2cf85d
ruslan <greenruslan@gmail.com>
2018-06-24 18:54:55 +0300

    test commit

--- test2.py
+++ test2.py
@@ -5,18 +5,7 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome('driver/chromedriver')
 driver.implicitly_wait(30)
 driver.maximize_window()
-driver.get("http://www.gmail.com")
-
-driver.find_element(By.XPATH, '//*[@id="identifierId"]').click()
-driver.find_element(By.XPATH, '//*[@id="identifierId"]').clear()
-driver.find_element(By.XPATH, '//*[@id="identifierId"]').send_keys("testqwerty6970@gmail.com")
-driver.find_element_by_css_selector("#identifierNext > content > span").click()
-driver.find_element_by_css_selector(".whsOnd.zHQkBf[type=password]").send_keys("test_passWORD713")
-driver.find_element_by_css_selector("#passwordNext > content > span").click()
-driver.find_element(By.XPATH, '/html/body/div[7]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div[2]/div/div/div/div[1]'
-                              '/div/div[]')
-driver.quit()
-
+driver.delete_all_cookies()
 
 
 
